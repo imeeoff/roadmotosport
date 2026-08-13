863d74443493bec068d62b2b0d51ba82d12c2233
imeeoff <geo.polikarpov@gmail.com>
2026-08-13 16:02:43 +0300

    Fix att 3

---
+++ vercel.js
@@ -0,0 +1,10 @@
+{
+  "rewrites": [
+    { "source": "/главная", "destination": "/index.html" },
+    { "source": "/каталог", "destination": "/catalog.html" },
+    { "source": "/галерея", "destination": "/gallery.html" },
+    { "source": "/о-нас", "destination": "/about.html" },
+    { "source": "/контакты", "destination": "/contact.html" },
+    { "source": "/авторизация", "destination": "/login.html" }
+  ]
+}
